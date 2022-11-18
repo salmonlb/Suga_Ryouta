ac47b7d43af488343c966145d74850ef973036e5
MSI\secon <suga7974@gmail.com>
2022-11-18 10:49:21 +0900

    20221109のTaskAが完成

--- Suga_Ryouta/20221109/TaskA.c
+++ Suga_Ryouta/20221109/TaskA.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <>
+#include <time.h>
 
 int main()
 {
+    time_t time(time_t * time);
+
+    srand((unsigned int) time(NULL));
+
     int data[11];
     int datasize = sizeof data / sizeof data[0];
+
+    int result = -1;
+    int target = 5;
 
     for (int i = 0; i < datasize; i++){
             data[i] = i;
@@ -26,8 +33,14 @@ int main()
     }
 
     for (int i = 0; i < datasize; i++){
-        printf("%d\n", data[i]);
+        if (data[i] == target){
+            result = i;
+            break;
+        }
     }
+
+    printf("target of index = ");
+    printf("%d",result);
 
     return 0;
 }

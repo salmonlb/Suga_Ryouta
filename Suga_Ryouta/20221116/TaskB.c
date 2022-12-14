8a67d6083504c193f696dc214aff4e7de1518be7
MSI\secon <suga7974@gmail.com>
2022-12-14 15:56:36 +0900

    ・20221116にTaskBを追加 ・TaskBの完成

---
+++ Suga_Ryouta/20221116/TaskB.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAX_QUEUE 5
+
+int Queue[MAX_QUEUE];
+int length = 0;
+
+void enqueue(int value);
+int dequeue();
+void showqueue();
+
+int main()
+{
+    enqueue(1);
+    showqueue();
+    enqueue(3);
+    showqueue();
+    enqueue(5);
+    showqueue();
+    enqueue(7);
+    showqueue();
+    enqueue(9);
+    showqueue();
+
+    printf("%d\n",dequeue());
+    showqueue();
+    printf("%d\n",dequeue());
+    showqueue();
+
+    return 0;
+}
+
+void enqueue(int value){
+    Queue[length] = value;
+    length += 1;
+}
+
+int dequeue(){
+    int rvalue;
+    rvalue = Queue[0];
+    length -= 1;
+    for (int i = 0; i < length; i++){
+        Queue[i] = Queue[i + 1];
+    }
+
+    return rvalue;
+}
+
+void showqueue(){
+    printf("Queue Array is : ");
+    for (int i = 0; i < length; i++){
+        printf("%d",Queue[i]);
+    }
+    printf("\n");
+}
